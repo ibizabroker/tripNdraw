8a34dc1067a76e37e5371a292086d1b5bb49c450
tarun gowda <tarun.gowda2000@gmail.com>
2023-08-18 19:29:17 +0530

    draw and reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [dots, setDots] = useState([]);
+
+  const drawDot = (event) => {
+    if (!isDrawing) return;
+
+    const color = getRandomColor();
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const newDot = {
+      color,
+      x,
+      y,
+    };
+
+    setDots((prevDots) => [...prevDots, newDot]);
+  };
+
+  const startDrawing = () => {
+    setIsDrawing(true);
+  };
+
+  const stopDrawing = () => {
+    setIsDrawing(false);
+  };
+
+  const resetDrawing = () => {
+    setDots([]);
+  };
+
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="reset-button" onClick={resetDrawing}>Reset</button>
+      <div
+        className="interactive-area"
+        onMouseDown={startDrawing}
+        onMouseUp={stopDrawing}
+        onMouseMove={drawDot}
+      >
+        {dots.map((dot, index) => (
+          <div
+            key={index}
+            className="colored-dot"
+            style={{
+              backgroundColor: dot.color,
+              left: dot.x + "px",
+              top: dot.y + "px",
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
