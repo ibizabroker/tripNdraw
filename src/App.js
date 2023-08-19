5280e75d2bc6dfaedd7ff174f21913a69315b92f
tarun gowda <tarun.gowda2000@gmail.com>
2023-08-19 12:04:00 +0530

    pencil and eraser options

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [tool, setTool] = useState("draw");
   const [isDrawing, setIsDrawing] = useState(false);
   const [dots, setDots] = useState([]);
+  const [eraserSize, setEraserSize] = useState(10);
+
+  const handleDraw = () => {
+    setTool("draw");
+  };
+
+  const handleErase = () => {
+    setTool("erase");
+  };
+
+  const adjustEraserSize = (event) => {
+    setEraserSize(parseInt(event.target.value));
+  };
 
   const drawDot = (event) => {
     if (!isDrawing) return;
 
-    const color = getRandomColor();
-    const x = event.clientX;
+    const color = tool === "erase" ? "#ffffff" : getRandomColor();
+    const x = event.clientX - 80; // Adjust for left panel width
     const y = event.clientY;
 
     const newDot = {
@@ -33,6 +47,20 @@ function App() {
     setDots([]);
   };
 
+  const eraseDots = (event) => {
+    if (!isDrawing || tool !== "erase") return;
+
+    const x = event.clientX - 80; // Adjust for left panel width
+    const y = event.clientY;
+
+    setDots((prevDots) =>
+      prevDots.filter(
+        (dot) =>
+          Math.sqrt((dot.x - x) ** 2 + (dot.y - y) ** 2) > eraserSize
+      )
+    );
+  };
+
   const getRandomColor = () => {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -42,14 +70,45 @@ function App() {
     return color;
   };
 
+  const cursorStyle = tool === "draw" ? "pointer" : "crosshair";
+
   return (
     <div className="App">
-      <button className="reset-button" onClick={resetDrawing}>Reset</button>
+      <div className="left-panel">
+        <button
+          onClick={handleDraw}
+          className={`tool-btn ${tool === "draw" ? "active" : ""}`}
+        >
+          🖋️
+        </button>
+        <button
+          onClick={handleErase}
+          className={`tool-btn ${tool === "erase" ? "active" : ""}`}
+        >
+          🧽
+        </button>
+        {tool === "erase" && (
+          <input
+            type="range"
+            min="1"
+            max="50"
+            value={eraserSize}
+            onChange={adjustEraserSize}
+          />
+        )}
+      </div>
+      <button className="reset-button" onClick={resetDrawing}>
+        Reset
+      </button>
       <div
         className="interactive-area"
         onMouseDown={startDrawing}
         onMouseUp={stopDrawing}
-        onMouseMove={drawDot}
+        onMouseMove={(event) => {
+          drawDot(event);
+          eraseDots(event);
+        }}
+        style={{ cursor: cursorStyle }}
       >
         {dots.map((dot, index) => (
           <div
